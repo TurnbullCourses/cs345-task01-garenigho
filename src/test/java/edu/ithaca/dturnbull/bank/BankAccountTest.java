b5f2915fb367827fa8492ab802660eea8308e384
garenigho <garenigho8@gmail.com>
2025-01-24 10:45:33 -0500

    Added tests to is EmailValidTest

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -30,7 +30,8 @@ class BankAccountTest {
         assertFalse(BankAccount.isEmailValid("renn@ithaca"));      // no ".com" 
         assertFalse(BankAccount.isEmailValid("@ithaca.com")); // no text before @
         assertFalse(BankAccount.isEmailValid("garenigho@gmailcom")); // no "." before "com"
-        
+        assertFalse(BankAccount.isEmailValid("_k@gmail.com"));  // cannot start with a special character
+        assertFalse(BankAccount.isEmailValid("g@.com"));       // needs to have text before "."
     }
 
     @Test
